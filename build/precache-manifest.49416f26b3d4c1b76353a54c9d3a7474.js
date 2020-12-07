0c730917ab06da70bba6e38992b13f9f96b97252
Franky Viray <harrypotterfan6969@yahoo.com>
2020-12-07 09:30:40 -0800

    did a npm run build and a npm install

---
+++ build/precache-manifest.49416f26b3d4c1b76353a54c9d3a7474.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b0c7a8033bb29c0c1006d0e714a02ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba9521afc31bbb6b618c",
+    "url": "/static/css/main.28016c21.chunk.css"
+  },
+  {
+    "revision": "d5c059ca813bb95fbadc",
+    "url": "/static/js/2.0b820256.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.0b820256.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba9521afc31bbb6b618c",
+    "url": "/static/js/main.f328e988.chunk.js"
+  },
+  {
+    "revision": "d3824011f0b19731245b",
+    "url": "/static/js/runtime-main.278d1234.js"
+  },
+  {
+    "revision": "63674950bfa71877566bd3a85b0f663e",
+    "url": "/static/media/ace-logo.63674950.png"
+  },
+  {
+    "revision": "f2c7a403daaea31b5550879151e7cfd5",
+    "url": "/static/media/acefilmstudio.f2c7a403.png"
+  }
+]);
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -4,20 +4,20 @@ import acelogo from "../../Images/ace-logo.png";
 // import acelogoText from "../../Images/ace-logo-txt-white.png";
 
 
-const Main = (props, mainRef) =>{
-    
-    return(
-        <div className="main" ref ={ mainRef }>            
-           <div className="main-title">
-               {/* <h1><span>ACE FILM STUDIOS</span></h1>     */}
-               <div className="logo-img">
+const Main = (props, mainRef) => {
+
+    return (
+        <div className="main" ref={mainRef}>
+            <div className="main-title">
+                {/* <h1><span>ACE FILM STUDIOS</span></h1>     */}
+                <div className="logo-img">
                     <p>WEBSITE COMING SOON</p>
-                    <img src={ acelogo } alt="ACE FILM STUDIOS" className="ace-logo-movie"/><br />          
-               </div>
-               
-               <p>television, Motion Picture <br />& digital media production</p>
-           </div>        
-        </div>  
+                    <img src={acelogo} alt="ACE FILM STUDIOS" className="ace-logo-movie" /><br />
+                </div>
+
+                <p>television, Motion Picture <br />& digital media production.</p>
+            </div>
+        </div>
     );
 }
 
